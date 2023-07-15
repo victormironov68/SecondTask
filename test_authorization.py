0a148837d88d64214de668bf75b3a35764191a3c
victormironov68 <bart.68mail@gmail.com>
2023-07-15 17:28:38 +0300

    в файле test_authorization.py.py рейлизована функия войти

--- test_authorization.py
+++ test_authorization.py
@@ -1,8 +1,27 @@
 import pytest
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_authorization(browser):
     link = "https://stepik.org/lesson/236895/step/1"
     browser.get(link)
-    browser.find_element()
+
+    time.sleep(3)
+
+    login_button = browser.find_element(By.XPATH, "//*[@id='ember33']")
+    login_button.click()
+
+    time.sleep(3)
+
+    email_field = browser.find_element(By.XPATH, "//*[@id='id_login_email']")
+    email_field.send_keys("bart.68mail@gmail.com")
+
+    password_field = browser.find_element(By.XPATH, "//*[@id='id_login_password']")
+    password_field.send_keys("tuttyfrutty")
+
+    input_button = browser.find_element(By.XPATH, "//*[@id='login_form']/button")
+    input_button.click()
+
+
+
