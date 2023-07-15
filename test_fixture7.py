476b8dfea40033f75fc851315fa6c1d62dee75c4
victormironov68 <bart.68mail@gmail.com>
2023-07-15 17:10:44 +0300

    Добавлен файл test_fixture7.py

--- test_fixture7.py
+++ test_fixture7.py
@@ -1,17 +1,21 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# @pytest.fixture(scope="function")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome()
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
+
 
 @pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+# def test_guest_should_see_login_link(browser, language):
+#     link = f"http://selenium1py.pythonanywhere.com/{language}/"
+#     browser.get(link)
+#     browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # этот тест запустится 2 раза
+
+    def test_guest_should_see_navbar_element(self, browser, language):
+        # этот тест тоже запустится дважды
+        pass
